92c36128e99706306873172a5b7b6fa8c11bfed8
Nahed5M <nahed.ksu@gmail.com>
2024-10-21 14:29:33 +0300

    Initial Commit

---
+++ Learning Journey/Learning_JourneyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Learning_JourneyApp.swift
+//  Learning Journey
+//
+//  Created by Nahed Almutairi on 18/04/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct Learning_JourneyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
